79ae9b426204ad6c5d5273b89d459b47d120cf13
Amulyas369 <amulyafood369@gmail.com>
2022-12-18 19:54:35 +0530

    hojababa

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,30 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-// class App extends Component {
-//     constructor(props) {
-// 		super(props);
-// 	};
-function App(){
-	const [displayMessage,setDisplayMessage]=useState(false);
+class App extends Component {
+    constructor(props) {
+		super(props);
+		this.state={display:false};
+		this.handleClick=this.handleClick.bind(this)
+	};
+// function App(){
+// 	const [displayMessage,setDisplayMessage]=useState(false);
+handleClick(){
+	this.setState({display:true});
+}
 
-
-    // render() {
+    render() {
 
     	return(
     		<div id="main">
-				{ (displayMessage===false)?(null):(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. this makes me so happy</p>) }
-				<button id="click" onClick={()=>setDisplayMessage(true)}>Click me</button>
+				{/* { this.state.display===true?<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:" } */}
+				<button id="click" onClick={this.handleClick}>Click me</button>
+				{ this.state.display===true?<p id='para'>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:"}
     		</div>
     	);
     }
 
-
+	}
 
 export default App;
 
